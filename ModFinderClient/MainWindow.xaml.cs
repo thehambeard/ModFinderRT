663343a6c0c367b36e095df0979e5b0e2d5af644
CasDragon <redyugi112@gmail.com>
2024-06-19 11:31:16 -0400

    Added in Barley's PR for mod version handle, Barley the real MVP

--- ModFinderClient/MainWindow.xaml.cs
+++ ModFinderClient/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ using System.Xml.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Data;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace ModFinder
 {
@@ -60,7 +61,7 @@ namespace ModFinder
         }
 #else
         Logger.Log.Verbose("Fetching remote manifest.");
-        var json = HttpHelper.GetResponseContent("https://raw.githubusercontent.com/CasDragon/ModFinder/main/ManifestUpdater/Resources/master_manifest.json");
+        var json = HttpHelper.GetResponseContent("https://raw.githubusercontent.com/CasDragon/ModFinder/RogueTrader/ManifestUpdater/Resources/master_manifest.json");
         Manifest = IOTool.FromString<MasterManifest>(json);
 #endif
 
@@ -128,7 +129,7 @@ namespace ModFinder
                 long latest = ParseVersion(tag.GetString()[1..]);
                 var fileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
                 var productVersion = fileVersion.ProductVersion;
-                if (productVersion.EndsWith("-dev"))
+                if (productVersion.Contains("-dev"))
                 {
                   return;
                 }
@@ -158,7 +159,8 @@ namespace ModFinder
 
     private static long ParseVersion(string v)
     {
-      v = v.Replace("-rel", "");
+      v = Regex.Replace(v, "-rel.*", "");
+      Logger.Log.Info("Version string equals - " + v);
       var c = v.Split('.');
       return int.Parse(c[0]) * 65536 + int.Parse(c[1]) * 256 + int.Parse(c[2]);
     }
